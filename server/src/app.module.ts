3b2d2fcb76e2671f415b7b6a8c72d32f5a46d4be
Denver <74683888+denver2202@users.noreply.github.com>
2025-06-22 14:53:32 +0300

    connect Redis

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,13 +2,15 @@ import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
 import { IS_DEV_ENV } from '@/libs/common/utils/is-dev.util'
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			ignoreEnvFile: !IS_DEV_ENV,
 			isGlobal: true
-		})
+		}),
+		PrismaModule
 	]
 })
 export class AppModule {}
--- server/src/main.ts
+++ server/src/main.ts
@@ -2,18 +2,46 @@ import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
+import * as session from 'express-session'
+import IORedis from 'ioredis'
+
+import { ms, StringValue } from '@/libs/common/utils/ms.util'
+import { parseBoolean } from '@/libs/common/utils/parse-boolean.utils'
 
 import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	const config = app.get(ConfigService)
+	const redis = new IORedis(config.getOrThrow('REDIS_URL'))
+	const connectRedis = require('connect-redis')
+	const RedisStore = connectRedis(session)
 
 	app.use(cookieParser(config.getOrThrow<string>('COOKIE_SECRET')))
 
 	app.useGlobalPipes(
 		new ValidationPipe({
 			transform: true
+		})
+	)
+
+	app.use(
+		session({
+			secret: config.getOrThrow<string>('SESSION_SECRET'),
+			name: config.getOrThrow<string>('SESSION_NAME'),
+			resave: true,
+			saveUninitialized: false,
+			cookie: {
+				domain: config.getOrThrow<string>('SESSION_DOMAIN'),
+				maxAge: ms(config.getOrThrow<StringValue>('SESSION_MAX_AGE')),
+				httpOnly: parseBoolean(config.getOrThrow<string>('SESSION_HTTP_ONLY')),
+				secure: parseBoolean(config.getOrThrow<string>('SESSION_SECURE')),
+				sameSite: 'lax'
+			},
+			store: new RedisStore({
+				client: redis,
+				prefix: config.getOrThrow<string>('SESSION_FOLDER')
+			})
 		})
 	)
 
